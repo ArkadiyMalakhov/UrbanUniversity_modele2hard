0a8c0c3815633d825dcc1fad8c5206bd9e091bd1
AMalakhov <spamarkady@yandex.ru>
2024-06-04 21:16:49 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,13 @@
+def gen_psw(n):
+    psw = ''
+    for i in range(1, n):
+         for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                psw += str(i) + ' ' + str(j) + ', '
+    return psw
+for n in range(3, 21):
+    psw = gen_psw(n)[:-1]
+    psw = psw[:-1]
+    print(f'{n} - {psw}')
+
+
